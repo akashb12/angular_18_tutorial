3070da17874dc4bb48bfba13924d9d28fce6d227
akash banaulikar <akash.banaulikar@adit.com>
2025-03-31 16:24:18 +0530

    api integration

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName:string = 'akash';
-  age:number = 28;
-  currentDate:Date = new Date();
+export class RolesComponent implements OnInit {
+  // firstName:string = 'akash';
+  // age:number = 28;
+  // currentDate:Date = new Date();
+  roleList: IRole[] = [];
+  http = inject(HttpClient)
+  ngOnInit(): void {
+    this.getAllRoles()
+  }
 
-  showWelcomeMessage() {
-    alert("welcome to angular 18 tutorial")
+  getAllRoles() {
+    this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles').subscribe((res: any) => {
+      this.roleList = res.data
+    })
   }
-  showAlertMessage(message:string) {
-    alert(message)
-  }
+
+  // showWelcomeMessage() {
+  //   alert("welcome to angular 18 tutorial")
+  // }
+  // showAlertMessage(message:string) {
+  //   alert(message)
+  // }
 }
