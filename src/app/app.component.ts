a5a7c9d61a351738cc8d061e43d5372b8b16be52
akash banaulikar <akash.banaulikar@adit.com>
2025-03-31 16:02:28 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RolesComponent } from './roles/roles.component';
+import { MasterComponent } from './master/master.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RolesComponent],
+  imports: [MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
